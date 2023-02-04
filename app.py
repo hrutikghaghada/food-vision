39482c3864539ebe9bc2f189112ef3c09abc2632
Hrutik2318 <sonihrithik60@gmail.com>
2023-02-04 20:55:00 +0530

    deployment related files

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+
+import tensorflow as tf
+from flask import Flask, app, render_template, request
+from werkzeug.exceptions import (Forbidden, HTTPException, NotFound,
+                                 RequestTimeout, Unauthorized)
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Load the model
+model = tf.keras.models.load_model("efficientnetB0_fine_tune")
+
+class_names = ['apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare', 'beet_salad', 'beignets', 'bibimbap', 'bread_pudding', 'breakfast_burrito', 'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake', 'ceviche', 'cheesecake', 'cheese_plate', 'chicken_curry', 'chicken_quesadilla', 'chicken_wings', 'chocolate_cake', 'chocolate_mousse', 'churros', 'clam_chowder', 'club_sandwich', 'crab_cakes', 'creme_brulee', 'croque_madame', 'cup_cakes', 'deviled_eggs', 'donuts', 'dumplings', 'edamame', 'eggs_benedict', 'escargots', 'falafel', 'filet_mignon', 'fish_and_chips', 'foie_gras', 'french_fries', 'french_onion_soup', 'french_toast', 'fried_calamari', 'fried_rice', 'frozen_yogurt', 'garlic_bread', 'gnocchi',
+               'greek_salad', 'grilled_cheese_sandwich', 'grilled_salmon', 'guacamole', 'gyoza', 'hamburger', 'hot_and_sour_soup', 'hot_dog', 'huevos_rancheros', 'hummus', 'ice_cream', 'lasagna', 'lobster_bisque', 'lobster_roll_sandwich', 'macaroni_and_cheese', 'macarons', 'miso_soup', 'mussels', 'nachos', 'omelette', 'onion_rings', 'oysters', 'pad_thai', 'paella', 'pancakes', 'panna_cotta', 'peking_duck', 'pho', 'pizza', 'pork_chop', 'poutine', 'prime_rib', 'pulled_pork_sandwich', 'ramen', 'ravioli', 'red_velvet_cake', 'risotto', 'samosa', 'sashimi', 'scallops', 'seaweed_salad', 'shrimp_and_grits', 'spaghetti_bolognese', 'spaghetti_carbonara', 'spring_rolls', 'steak', 'strawberry_shortcake', 'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna_tartare', 'waffles']
+
+
+def load_and_prep_image(img_path, img_shape=224, scale=True):
+    """
+    Reads in an image from img_path, turns it into a tensor and reshapes into
+    (224, 224, 3).
+
+    Parameters
+    ----------
+    img_path (str): string filename of target image
+    img_shape (int): size to resize target image to, default 224
+    scale (bool): whether to scale pixel values to range(0, 1), default True
+    """
+    # Read in the image
+    img = tf.io.read_file(img_path)
+    # Decode it into a tensor
+    img = tf.io.decode_image(img)
+    # Resize the image
+    img = tf.image.resize(img, [img_shape, img_shape])
+    if scale:
+        # Rescale the image (get all values between 0 and 1)
+        return img/255.
+    else:
+        return img
+
+
+def model_predict(img_path, model):
+    img = load_and_prep_image(img_path, scale=False)
+
+    pred_prob = model.predict(tf.expand_dims(img, axis=0))
+    pred_class = class_names[pred_prob.argmax()]
+
+    return pred_class + f" with probability {tf.reduce_max(pred_prob):.2f}"
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        result = model_predict(file_path, model)
+        print(result)
+        return result
+    return None
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
